210498593453cbdd3ea754c13b50ea6bcaa47982
Povilas Pasausis <hello.povi@gmail.com>
2019-12-30 09:04:46 +0100

    Removes commented test case.

--- src/components/StationListItem/StationListItem.test.js
+++ src/components/StationListItem/StationListItem.test.js
@@ -36,30 +36,4 @@ describe('StationListItem component tests', () => {
       });
     });
   });
-
-  describe('expanded state', () => {
-    // describe('onClick event on radio station', () => {
-    //   const station = {
-    //     id: 'radio1',
-    //     name: 'RelaxFM',
-    //     freq: '100',
-    //     logo: 'https://static-media.streema.com/media/cache/c3/c0/c3c006a91b3d49470c8e4d6ab608c3ca.jpg',
-    //     isActive: true
-    //   };
-    //   const div = document.createElement('main');
-    //   const callback = jest.fn();
-    //   const { getByTitle, container } = render(<StationListItem {...station} onClick={callback} />, div);
-    // const stationButton = getByTitle('Select station');
-    // fireEvent.click(stationButton);
-    // it('calls callback once', () => {
-    //   expect(callback).toHaveBeenCalledTimes(1);
-    // });
-    // it('sets right css classes', () => {
-    //   waitForElement(() => expect(container.firstChild).toHaveClass('stations-list-item'));
-    // });
-    // it('sets accessibility tags', () => {
-    //   waitForElement(() => expect(container.firstChild).toHaveAttribute('area-expanded', 'false'));
-    // });
-    // });
-  });
 });
